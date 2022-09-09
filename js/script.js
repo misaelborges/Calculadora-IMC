30155eeaf65b49efb427d9dd1da98cf10e4f55b3
misael borges cancelier <mizaelborges2011@gmail.com>
2022-09-09 16:09:40 -0300

    Correção dos erros causado pelos valores invalidos

--- js/script.js
+++ js/script.js
@@ -7,18 +7,22 @@ function clicar() {
     let imc = Number(peso) / (Number(altura)**2)
     let agua = Number(peso)*0.035
     
-    res.innerHTML += `<strong>${imc.toFixed(1)}</strong>`
-    if (imc < 18.5) {
-        msg.innerHTML = 'Segundo a OMS(Organização mundial da saúde) seu IMC indica: <strong>Magreza</strong>'
-    } else if (imc < 25) {
-        msg.innerHTML = 'Segundo a OMS(Organização mundial da saúde) seu IMC esta: <strong>Normal</strong>'
-    } else if (imc < 30) {
-        msg.innerHTML = 'Segundo a OMS(Organização mundial da saúde) seu IMC indica: <strong>Sobrepeso</strong> nivel de OBESIDADE I'
-    } else if (imc < 40) {
-        msg.innerHTML = 'Segundo a OMS(Organização mundial da saúde) seu IMC indica: <strong>OBESIDADE II</strong>'
+    if ((altura < 0) || (peso < 0)) {
+        window.alert('[ERRO] Valores invalidos')
     } else {
-        msg.innerHTML = 'Segundo a OMS(Organização mundial da saúde) seu IMC indica: <strong>OBESIDADE GRAVE III</strong>'
+        res.innerHTML += `<strong>${imc.toFixed(1)}</strong>`
+        if (imc < 18.5) {
+            msg.innerHTML = 'Segundo a OMS(Organização mundial da saúde) seu IMC indica: <strong>Magreza</strong>'
+        } else if (imc < 25) {
+            msg.innerHTML = 'Segundo a OMS(Organização mundial da saúde) seu IMC esta: <strong>Normal</strong>'
+        } else if (imc < 30) {
+            msg.innerHTML = 'Segundo a OMS(Organização mundial da saúde) seu IMC indica: <strong>Sobrepeso</strong> nivel de OBESIDADE I'
+        } else if (imc < 40) {
+            msg.innerHTML = 'Segundo a OMS(Organização mundial da saúde) seu IMC indica: <strong>OBESIDADE II</strong>'
+        } else {
+            msg.innerHTML = 'Segundo a OMS(Organização mundial da saúde) seu IMC indica: <strong>OBESIDADE GRAVE III</strong>'
+        }
+        complemento.innerHTML = `Sugerimos a você tomar ${agua.toFixed(1)} litros de agua por dia`
     }
-    complemento.innerHTML = `Sugerimos a você tomar ${agua.toFixed(1)} litros de agua por dia`
 }
 
